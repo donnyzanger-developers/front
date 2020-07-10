0d512de3c0ce0d6fd976bdf74b16dee65eef899c
JamesSchiller <schillerj78@gmail.com>
2020-07-10 06:09:19 -0400

    developing admin panel

--- admin.js
+++ admin.js
@@ -19,6 +19,48 @@ async function main() {
             window.location = '/index.html';
         }
     }
+    const res = await fetch(API + '/users', {
+        headers: {
+            'Authorization': localStorage.getItem('googleToken')
+        }
+    })
+    const users = await res.json();
+    var html = `
+        <div class="row">
+            <div class="col-3">email</div>
+            <div class="col-1">paid</div>
+            <div class="col-1">admin</div>
+            <div class="col-3">date</div>
+            <div class="col-4">
+                operations
+            </div>
+        </div>`
+    users.forEach((user, i) => {
+        html += ` 
+            <div class="row">
+                <div class="col-3">${user.email}</div>
+                <div class="col-1">${user.paid}</div>
+                <div class="col-1">${user.admin}</div>
+                <div class="col-3">${user.created}</div>
+                <div class="col-4">
+                    <button id="edit-${i}">edit</button>
+                    <button id="delete-${i}">delete</button>
+                </div>
+            </div>`
+    });
+    document.getElementById('user-table').innerHTML = html;
+    users.forEach((user, i) => {
+    // for (var i=0; i < users.length; i++) {
+        document.getElementById(`edit-${i}`).addEventListener('click', async e => {
+            window.location =`/user.html?id=${user._id}`;
+        });
+        document.getElementById(`delete-${i}`).addEventListener('click', async e => {
+            alert(i);
+            // delete user i via api
+            // page should show deleted user is gone
+            // 
+        });
+    });
 }
 
 main();
---
+++ user.js
@@ -0,0 +1,15 @@
+var url = new URL(window.location.href);
+var id = url.searchParams.get("id");
+
+async function main() {
+    const res = await fetch(API + `/users/${id}`, {
+        headers: {
+            'Authorization': localStorage.getItem('googleToken')
+        }
+    })
+    const user = await res.json();
+
+    document.getElementById('email').innerHTML = user.email;
+}
+
+main();
