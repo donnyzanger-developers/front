63da88d1e333396fcaa6142504d966d2bcc41397
JamesSchiller <schillerj78@gmail.com>
2020-07-10 23:10:08 -0400

    admin panel

--- user.js
+++ user.js
@@ -8,8 +8,27 @@ async function main() {
         }
     })
     const user = await res.json();
-
     document.getElementById('email').innerHTML = user.email;
+    document.getElementById('paid').value = user.paid;
+    document.getElementById('save').addEventListener('click', async e => {
+        var paid = document.getElementById('paid').value;
+        const res = await fetch(API + '/admin/users/' + id, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': localStorage.getItem('googleToken')
+            },
+            body: JSON.stringify({
+              paid: paid
+            })
+        })
+        const updatedUser = await res.json();
+        window.location = '/admin.html';
+    });
 }
 
 main();
+
+// document.getElementById('paid').addEventListener('change', e => {
+// alert(document.getElementById('paid').value)
+// });
